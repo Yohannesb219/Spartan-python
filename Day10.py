482dd3dad208acd393b1e068c68429ca2596f686
Yohannesb219 <yohannesb777@gmail.com>
2026-02-26 19:47:19 +0300

    Refactor number input handling in Day10.py

--- Day10.py
+++ Day10.py
@@ -1,5 +1,18 @@
-numbers=[1,8,9,5,6,4,8,7,6,3,6,4,9,7,6]
+numbers=[]
+while True:
+    operation=input("Enter purpose (Add/Stop)").lower()
+    if operation=="add":
+        num=int(input("Enter number:"))
+        numbers.append(num)
+    elif operation=="stop":
+        break
+    else:
+        print("Invalid input")
+    
+
 def analyze(numbers):
+    if not numbers:
+        return None,None,None
     large=numbers[0]
     small=numbers[0]
     total=0
@@ -11,8 +24,7 @@ def analyze(numbers):
         total +=num
     average= total/ len(numbers)
     return large,small,average
-average,large,small=analyze(numbers)
+large,small,average=analyze(numbers)
 print("max",large)
 print("min",small)
 print("average",average)
-    
